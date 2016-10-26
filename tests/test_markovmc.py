511446dec639495ace5bbfb536642b20344e8c03
Dominic Giambra <dgiambra@u.rochester.edu>
2016-10-26 14:05:56 -0400

    Final Project Updates

--- tests/test_markovmc.py
+++ tests/test_markovmc.py
@@ -13,10 +13,12 @@ import sys
 import unittest
 from contextlib import contextmanager
 from click.testing import CliRunner
-
+import networkx as nx
 from markovmc import markovmc
 
-
+Z = nx.Graph()
+Z.add_edge(1,2,weight = 2)
+A = [Z]
 
 
 class TestMarkovmc(unittest.TestCase):
@@ -69,8 +71,17 @@ class TestMarkovmc(unittest.TestCase):
     def test_expectedDegree_output(self):
         self.assertIsInstance(markovmc.expectedDegree(markovmc.grapher([(0,0),(1,2),(2,2),(3,3),(4,4),(5,5),(6,6)],20,1,1),0), float)
 
+    def test_expectedDegree_single(self):
+        self.assertTrue(markovmc.expectedDegree(A,1)==1)
+
     def test_expectedNumberofEdges_output(self):
         self.assertIsInstance(markovmc.expectedNumberofEdges(markovmc.grapher([(0,0),(1,2),(2,2),(3,3),(4,4),(5,5),(6,6)],20,1,1)), float)
 
+    def test_expectedNumberofEdges_single(self):
+        self.assertTrue(markovmc.expectedNumberofEdges(A)==1)
+
     def test_expectedShartedtPathLength(self):
         self.assertIsInstance(markovmc.expectedShortestPathLength(markovmc.grapher([(0,0),(1,2),(2,2),(3,3),(4,4),(5,5),(6,6)],20,1,1),0,6), float)
+
+    def test_expectedShortestPathLength_single(self):
+        self.assertTrue(markovmc.expectedShortestPathLength(A,2,1)==2)
